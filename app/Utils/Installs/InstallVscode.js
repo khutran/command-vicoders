e7f006177e2158c561cb2c36b531aa18b7555d59
khutran <xuankhu1111@gmail.com>
2018-08-27 17:17:28 +0700

    install vscode in mac , linux

--- app/Utils/Installs/InstallVscode.js
+++ app/Utils/Installs/InstallVscode.js
@@ -49,6 +49,7 @@ export default class InstallVscode extends Install {
         const linux = new Linux();
         const osName = linux.osName();
         if (osName === 'debian') {
+          const data = 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main';
           const microsoft = spawn('curl', ['https://packages.microsoft.com/keys/microsoft.asc']);
           const gpg = spawn('gpg', ['--dearmor', '>', 'microsoft.gpg']);
           microsoft.stdout.on('data', data => {
@@ -58,6 +59,25 @@ export default class InstallVscode extends Install {
           gpg.on('close', code => {
             if (code === 0) {
               console.log(colors.green('down microsoft success ... done !'));
+              fs.appendFile('/etc/apt/sources.list.d/vscode.list', data, err => {
+                if (err) {
+                  throw new Exception('create file repo errro');
+                }
+                console.log(colors.green('create file repo ... success !'));
+              });
+            }
+          });
+          await exec('apt-get -y update');
+          const code = spawn('apt-get', ['-y', 'install', 'code']);
+          code.stdout.on('data', data => {
+            console.log(data.toString());
+          });
+          code.stderr.on('data', data => {
+            console.log(data.toString());
+          });
+          code.on('close', code => {
+            if (code === 0) {
+              console.log(colors.green('Install vs code success ... !'));
             }
           });
         }
