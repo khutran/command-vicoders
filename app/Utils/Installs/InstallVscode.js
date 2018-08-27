4e14f18f54294e195d9f6a1fd0bb6fd28e8db453
khutran <xuankhu1111@gmail.com>
2018-08-27 17:05:03 +0700

    update

--- app/Utils/Installs/InstallVscode.js
+++ app/Utils/Installs/InstallVscode.js
@@ -1,4 +1,5 @@
 import Install from './Install';
+import fs from 'fs';
 import { Exception } from '@codersvn/exceptions';
 import inquirer from 'inquirer';
 import colors from 'colors';
@@ -62,19 +63,13 @@ export default class InstallVscode extends Install {
         }
         if (osName === 'redhat') {
           await exec('rpm --import https://packages.microsoft.com/keys/microsoft.asc');
-          const addRepo = spawn('sh', [
-            '-c',
-            'echo',
-            '-e',
-            '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc',
-            '>',
-            '/etc/yum.repos.d/vscode.repo'
-          ]);
-
-          addRepo.on('close', code => {
-            if (code === 0) {
-              console.log(colors.green('add repo sucess ... !'), 1);
+          const data =
+            '[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc';
+          fs.appendFile('/etc/yum.repos.d/vscode.repo', data, err => {
+            if (err) {
+              throw new Exception(colors.red('create file repo error'), 1);
             }
+            console.log(colors.green('create file repo ... success !'));
           });
         }
       }
