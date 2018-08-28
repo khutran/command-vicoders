4fda7941dd12277bfa8e3b96938955b0c72988b5
khutran <xuankhu1111@gmail.com>
2018-08-28 09:45:12 +0700

    update check

--- app/Utils/Os/Darwin.js
+++ app/Utils/Os/Darwin.js
@@ -1,12 +1,16 @@
-import fs from 'fs';
-// import { Exception } from 'handlebars';
 import Os from './Os';
+import * as _ from 'lodash';
+import fs from 'fs';
 
 export default class Darwin extends Os {
   CheckExists(name) {
-    if (fs.existsSync(`/usr/local/bin/${name}`)) {
-      return true;
-    }
-    return false;
+    const folder = [`/usr/local/bin/${name}`, `/usr/bin/${name}`, `/usr/sbin/${name}`, `/usr/local/sbin/${name}`];
+    let result = false;
+    _.forEach(folder, item => {
+      if (fs.existsSync(item)) {
+        result = true;
+      }
+    });
+    return result;
   }
 }
