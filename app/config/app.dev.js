1e1c28f6aaf09a0d691ef79efa699020e54e8341
khutran <xuankhu1111@gmail.com>
2018-08-29 08:17:33 +0700

    cp

---
+++ app/config/app.dev.js
@@ -0,0 +1,4 @@
+export const Config = {
+  use_db_commands: ['project', 'open'],
+  sql_lite_path: './data/vcc.db'
+};
--- models/index.js
+++ models/index.js
@@ -1,31 +1,38 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import { Config } from '../app/config/app';
+
 const basename = path.basename(module.filename);
-let db = {};
 
-const sequelize = new Sequelize('vcc', null, null, {
-  dialect: 'sqlite',
-  storage: './data/vcc.db',
-  logging: false
-});
+const db = {};
+const command = process.argv[2];
 
-fs.readdirSync(__dirname)
-  .filter(file => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+if (Config.use_db_commands.indexOf(command) > -1) {
+  const sequelize = new Sequelize('vcc', null, null, {
+    dialect: 'sqlite',
+    storage: Config.sql_lite_path,
+    logging: false,
+    operatorsAliases: false
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  fs.readdirSync(__dirname)
+    .filter(file => {
+      return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
+    })
+    .forEach(file => {
+      const model = sequelize['import'](path.join(__dirname, file));
+      db[model.name] = model;
+    });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
+
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
+}
 
 module.exports = db;
