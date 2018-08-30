7d6205cdbad0ec491957f2b8fcdd348209204ca0
khutran <xuankhu1111@gmail.com>
2018-08-30 13:41:07 +0700

    test

--- app/Utils/Installs/installNginx.js
+++ app/Utils/Installs/installNginx.js
@@ -13,7 +13,6 @@ const exec = util.promisify(require('child_process').exec);
 
 export default class installNginx extends Install {
   async service() {
-    console.log(this.os);
     if (this.os === 'darwin') {
       console.log('test');
       // try {
@@ -47,34 +46,34 @@ export default class installNginx extends Install {
       const linux = new Linux();
       const osName = linux.osName();
       if (osName === 'debian') {
-        // try {
-        //   const url = 'https://github.com/khutran/ubuntu-nginx/archive/1.13.8.zip';
-        //   const download = new Download();
-        //   const download_nginx = await download.form(url).to('/tmp');
-        //   const dest = path.dirname(download_nginx.filepath);
-        //   const extral = await decompress(download_nginx.filepath, dest);
-        //   mv(`${dest}/${extral[0].path}usr-nginx`, '/usr/local/nginx', { mkdirp: true }, err => {
-        //     if (err) {
-        //       throw new Exception(err.message, 1);
-        //     }
-        //   });
-        //   mv(`${dest}/${extral[0].path}etc-nginx`, '/etc/nginx', { mkdirp: true }, err => {
-        //     if (err) {
-        //       throw new Exception(err.message, 1);
-        //     }
-        //   });
-        //   mv(`${dest}/${extral[0].path}nginx.service`, '/lib/systemd/system//nginx.service', { mkdirp: true }, err => {
-        //     if (err) {
-        //       throw new Exception(err.message, 1);
-        //     }
-        //   });
-        //   fs.symlinkSync('/usr/sbin/nginx', '/usr/local/nginx/bin/nginx');
-        //   await exec('systemctl daemon-reload');
-        //   await rimraf(download_nginx.filepath);
-        //   await rimraf(`${dest}/${extral[0].path}`);
-        // } catch (e) {
-        //   throw new Exception(e.message, 1);
-        // }
+        try {
+          const url = 'https://github.com/khutran/ubuntu-nginx/archive/1.13.8.zip';
+          const download = new Download();
+          const download_nginx = await download.form(url).to('/tmp');
+          const dest = path.dirname(download_nginx.filepath);
+          const extral = await decompress(download_nginx.filepath, dest);
+          mv(`${dest}/${extral[0].path}usr-nginx`, '/usr/local/nginx', { mkdirp: true }, err => {
+            if (err) {
+              throw new Exception(err.message, 1);
+            }
+          });
+          mv(`${dest}/${extral[0].path}etc-nginx`, '/etc/nginx', { mkdirp: true }, err => {
+            if (err) {
+              throw new Exception(err.message, 1);
+            }
+          });
+          mv(`${dest}/${extral[0].path}nginx.service`, '/lib/systemd/system//nginx.service', { mkdirp: true }, err => {
+            if (err) {
+              throw new Exception(err.message, 1);
+            }
+          });
+          fs.symlinkSync('/usr/sbin/nginx', '/usr/local/nginx/bin/nginx');
+          // await exec('systemctl daemon-reload');
+          await rimraf(download_nginx.filepath);
+          await rimraf(`${dest}/${extral[0].path}`);
+        } catch (e) {
+          throw new Exception(e.message, 1);
+        }
       }
     }
   }
