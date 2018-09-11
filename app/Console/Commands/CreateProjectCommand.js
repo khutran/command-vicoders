10faef535d7f6a9cd7f3aba0d9e1ca40732e8ee5
Hieupv <hieupv@codersvn.com>
2018-09-11 23:41:48 +0700

    update create project command

--- app/Console/Commands/CreateProjectCommand.js
+++ app/Console/Commands/CreateProjectCommand.js
@@ -5,11 +5,25 @@ import path from 'path';
 import rimraf from 'rimraf';
 import mv from 'mv';
 import fs from 'fs';
+import readline from 'readline';
 import { Decompresser } from '../../Utils/Decompresser';
+import { Exception } from '@nsilly/exceptions';
+
+const createFromUrl = async function(url, name) {
+  const filename = Date.now().toString();
+  const dest = path.resolve(process.cwd(), filename + '.zip');
+  await App.make(Downloader).download(url, dest);
+  await App.make(Decompresser).decompress(dest, path.resolve(process.cwd(), filename));
+  rimraf.sync(dest);
+  const folder = path.resolve(process.cwd(), filename);
+  const files = fs.readdirSync(folder);
+  mv(`${folder}/${files[0]}`, path.resolve(process.cwd(), name), () => {});
+  rimraf.sync(folder);
+};
 
 export default class CreateProjectCommand extends Command {
   signature() {
-    return 'create-project <type>';
+    return 'create-project';
   }
 
   description() {
@@ -20,34 +34,57 @@ export default class CreateProjectCommand extends Command {
     return [{ key: 'name', description: 'The application name' }];
   }
 
-  async handle(type, options) {
-    const createFromUrl = async function(url, name) {
-      const filename = Date.now().toString();
-      const dest = path.resolve(process.cwd(), filename + '.zip');
-      await App.make(Downloader).download(url, dest);
-      await App.make(Decompresser).decompress(dest, path.resolve(process.cwd(), filename));
-      rimraf.sync(dest);
-      if (typeof options.name === 'string' && options.name !== '') {
-        name = options.name;
+  async handle(options) {
+    const supported_kits = [
+      {
+        url: 'http://bitbucket.org/vicoderscom/vc_kit_angular_cli_v6/get/master.zip',
+        name: 'angular-admin',
+        description: 'Create an angular admin'
+      },
+      {
+        url: 'https://bitbucket.org/vicoderscom/vc_kit_html_css_javascript/get/master.zip',
+        name: 'kit-html-css',
+        description: 'Create a kit for HTML/CSS/JS project'
+      },
+      {
+        url: 'https://bitbucket.org/hieu_pv/vc_kit_javascript_module/get/master.zip',
+        name: 'kit-javascript',
+        description: 'Create a kit for javascript module development'
+      },
+      {
+        url: 'https://github.com/codersvn/kit_nodejs/archive/master.zip',
+        name: 'kit-nodejs',
+        description: 'Create a kit for nodejs module development'
       }
-      const folder = path.resolve(process.cwd(), filename);
-      const files = fs.readdirSync(folder);
-      mv(`${folder}/${files[0]}`, path.resolve(process.cwd(), name), () => {});
-      rimraf.sync(folder);
-    };
-    switch (type) {
-      case 'angular-admin':
-        await createFromUrl('http://bitbucket.org/vicoderscom/vc_kit_angular_cli_v6/get/master.zip', 'angular-admin');
-        break;
-      case 'kit-html-css':
-        await createFromUrl('https://bitbucket.org/vicoderscom/vc_kit_html_css_javascript/get/master.zip', 'kit-html-css');
-        break;
-      case 'kit-javascript':
-        await createFromUrl('https://bitbucket.org/hieu_pv/vc_kit_javascript_module/get/master.zip', 'kit-javascript');
-        break;
-      default:
-        break;
-    }
-    process.exit();
+    ];
+    return new Promise(async function(resolve, reject) {
+      try {
+        const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+        });
+
+        let message = 'Select your kit that you want \n';
+        supported_kits.forEach((item, key) => {
+          message += `\n ${key + 1}. ${item.description}`;
+        });
+        message += '\n \n Kit Number: ';
+        rl.question(message, async function(answer) {
+          rl.close();
+          const index = Number(answer) - 1;
+          if (index > supported_kits.length) {
+            throw new Exception('Please select your project that you want to start', 1);
+          }
+          let name = supported_kits[index].name;
+          if (typeof options.name === 'string' && options.name !== '') {
+            name = options.name;
+          }
+          await createFromUrl(supported_kits[index].url, name);
+          resolve();
+        });
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 }
