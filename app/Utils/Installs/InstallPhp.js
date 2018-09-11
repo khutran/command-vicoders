7b55607108fe14455a0c7be2a03815282283272c
khutran <xuankhu1111@gmail.com>
2018-09-11 16:44:50 +0700

    test

--- app/Utils/Installs/InstallPhp.js
+++ app/Utils/Installs/InstallPhp.js
@@ -29,11 +29,11 @@ export default class installPhp extends Install {
           let app = spawn('add-apt-repository', ['ppa:ondrej/php']);
           app.stdin.write('3');
           console.log('update .... !');
-          await exec('apt -y update');
+          await exec('apt-get -y update');
           console.log(`Install php ${version}`);
-          await exec(
-            `apt-get install -y php${version} php${version}-cli php${version}-common php${version}-json php${version}-opcache php${version}-mysql php${version}-mbstring php${version}-mcrypt php${version}-zip php${version}-fpm`
-          );
+          // await exec(
+          //   `apt-get install -y php${version} php${version}-cli php${version}-common php${version}-json php${version}-opcache php${version}-mysql php${version}-mbstring php${version}-mcrypt php${version}-zip php${version}-fpm`
+          // );
         } catch (e) {
           throw new Exception(e.message, 1);
         }
