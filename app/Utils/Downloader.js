7fafc0613ca4680e23ec66a2a72f284381fb8e1a
khutran <xuankhu1111@gmail.com>
2018-09-13 13:34:54 +0700

    test

--- app/Utils/Downloader.js
+++ app/Utils/Downloader.js
@@ -15,21 +15,23 @@ export class Downloader {
       options = { method: 'GET', responseType: 'stream', url: url };
     }
 
-    const response = await axios(options);
+    const response = axios(options);
 
     response.data.pipe(fs.createWriteStream(dest));
+
     return new Promise((resolve, reject) => {
       const len = parseInt(response.data.headers['content-length'], 10);
       let cur = 0;
-      // const total = len / 1048576;
+      const total = len / 1048576;
+
       process.stdout.write('Downloading ...');
 
       response.data.on('data', function(chunk) {
-        // cur += chunk.length;
-        // const percent = cur / len;
-        // process.stdout.clearLine();
-        // process.stdout.cursorTo(0);
-        // process.stdout.write(`Downloading ${cur}% of ${len}MB`);
+        cur += chunk.length;
+        const percent = ((100.0 * cur) / len).toFixed(2);
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(`Downloading ${percent}% of ${total.toFixed(2)}MB`);
       });
 
       response.data.on('end', () => {
