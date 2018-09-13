c304c5779a659340becab20c489f51e1f646b1ad
khutran <xuankhu1111@gmail.com>
2018-09-13 09:07:50 +0700

    test inquirer install service

--- app/Console/Commands/InstallServiceCommand.js
+++ app/Console/Commands/InstallServiceCommand.js
@@ -118,75 +118,76 @@ export default class VscodeCommand extends Command {
         });
         const answers = await inquirer.prompt({ type: 'input', name: 'service', message: 'select service want install : ', default: 1 });
         service = listService[answers.service];
-        switch (service) {
-          case 'vscode':
-            try {
-              const install = new InstallVscode();
-              const result = await install.service();
-              if (result.code === 1) {
-                console.log(colors.green(result.message));
-              }
-              if (!_.isUndefined(data.installExtentions)) {
-                await install.extentions();
-              }
-            } catch (e) {
-              console.log(colors.red(e.message));
-            }
-            break;
-          case 'subl':
-            try {
-              const install = new InstallSubl();
-              const result = await install.service();
-              if (result.code === 1) {
-                console.log(colors.green(result.message));
-              }
-              if (!_.isUndefined(data.installExtentions)) {
-                await install.extentions();
-              }
-            } catch (e) {
-              console.log(colors.red(e.message));
-            }
-            break;
-          case 'nginx':
-            try {
-              let version = '1.13.8';
-              if (!_.isUndefined(data.version)) {
-                version = data.version;
-              }
-              const install = new installNginx();
-              await install.service(version);
-              console.log(colors.green('success .. !'));
-            } catch (e) {
-              console.log(colors.red(e.message));
-            }
-            break;
-          case 'apache':
-            try {
-              let version = '2.4.34';
-              if (!_.isUndefined(data.version)) {
-                version = data.version;
-              }
-              const install = new installApache();
-              await install.service(version);
-              console.log(colors.green('success .. !'));
-            } catch (e) {
-              console.log(colors.red(e.message));
-            }
-            break;
-          case 'php':
-            try {
-              let version = '7.2';
-              if (!_.isUndefined(data.version)) {
-                version = data.version;
-              }
-              const install = new installPhp();
-              await install.service(version);
-              console.log(colors.green('success .. !'));
-            } catch (e) {
-              console.log(colors.red(e.message));
-            }
-            break;
-        }
+        console.log(service);
+        // switch (service) {
+        //   case 'vscode':
+        //     try {
+        //       const install = new InstallVscode();
+        //       const result = await install.service();
+        //       if (result.code === 1) {
+        //         console.log(colors.green(result.message));
+        //       }
+        //       if (!_.isUndefined(data.installExtentions)) {
+        //         await install.extentions();
+        //       }
+        //     } catch (e) {
+        //       console.log(colors.red(e.message));
+        //     }
+        //     break;
+        //   case 'subl':
+        //     try {
+        //       const install = new InstallSubl();
+        //       const result = await install.service();
+        //       if (result.code === 1) {
+        //         console.log(colors.green(result.message));
+        //       }
+        //       if (!_.isUndefined(data.installExtentions)) {
+        //         await install.extentions();
+        //       }
+        //     } catch (e) {
+        //       console.log(colors.red(e.message));
+        //     }
+        //     break;
+        //   case 'nginx':
+        //     try {
+        //       let version = '1.13.8';
+        //       if (!_.isUndefined(data.version)) {
+        //         version = data.version;
+        //       }
+        //       const install = new installNginx();
+        //       await install.service(version);
+        //       console.log(colors.green('success .. !'));
+        //     } catch (e) {
+        //       console.log(colors.red(e.message));
+        //     }
+        //     break;
+        //   case 'apache':
+        //     try {
+        //       let version = '2.4.34';
+        //       if (!_.isUndefined(data.version)) {
+        //         version = data.version;
+        //       }
+        //       const install = new installApache();
+        //       await install.service(version);
+        //       console.log(colors.green('success .. !'));
+        //     } catch (e) {
+        //       console.log(colors.red(e.message));
+        //     }
+        //     break;
+        //   case 'php':
+        //     try {
+        //       let version = '7.2';
+        //       if (!_.isUndefined(data.version)) {
+        //         version = data.version;
+        //       }
+        //       const install = new installPhp();
+        //       await install.service(version);
+        //       console.log(colors.green('success .. !'));
+        //     } catch (e) {
+        //       console.log(colors.red(e.message));
+        //     }
+        //     break;
+        // }
         break;
     }
   }
