589ee969b4e2b0a490d2e0b943a74fa489963a08
khutran <xuankhu1111@gmail.com>
2018-09-14 15:59:44 +0700

    update

--- app/Utils/Installs/installMysql.js
+++ app/Utils/Installs/installMysql.js
@@ -1,9 +1,7 @@
 import Install from './Install';
 import Linux from '../Os/Linux';
-import fs from 'fs';
 import colors from 'colors';
 import { dd } from 'dumper.js';
-import { exec } from 'child-process-promise';
 
 export default class installMysql extends Install {
   async service() {
@@ -13,16 +11,10 @@ export default class installMysql extends Install {
       const linux = new Linux();
       const osName = linux.osName();
       if (osName === 'debian') {
-        if (!fs.existsSync('/var/lib/mysql')) {
-          await exec('apt-get -y update');
-          await exec('apt-get install -y mysql-server');
-          await exec('/etc/init.d/mysql start');
-          await exec('mysql_secure_installation');
-        } else if ((await linux.CheckExists('mysql')) && fs.existsSync('/var/lib/mysql')) {
-          dd(colors.green('your computer installed mysql - run "apt install mysql-server"'));
-        }
+        dd(colors.green('apt install mysql-server'));
       }
       if (osName === 'redhat') {
+        dd(colors.green('yum install -y mysql-server'));
       }
     }
   }
