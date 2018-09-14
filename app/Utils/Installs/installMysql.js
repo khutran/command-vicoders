d6d1598cc71dd6ea842e622e18f0a448b0d0a339
khutran <xuankhu1111@gmail.com>
2018-09-14 14:44:12 +0700

    install mysql

--- app/Utils/Installs/installMysql.js
+++ app/Utils/Installs/installMysql.js
@@ -1,15 +1,9 @@
 import Install from './Install';
 import Linux from '../Os/Linux';
-import { Downloader } from '../Downloader';
 import fs from 'fs';
-import { App } from '@nsilly/container';
-import path from 'path';
 import colors from 'colors';
 import { dd } from 'dumper.js';
-import decompress from 'decompress';
-const util = require('util');
-const rimraf = util.promisify(require('rimraf'));
-const mv = util.promisify(require('mv'));
+import { exec } from 'child-process-promise';
 
 export default class installMysql extends Install {
   async service() {
@@ -20,13 +14,10 @@ export default class installMysql extends Install {
       const osName = linux.osName();
       if (osName === 'debian') {
         if (!(await linux.CheckExists('mysql')) && !fs.existsSync('/var/lib/mysql')) {
-          const url = 'https://github.com/khutran/mysql/archive/ubuntu.zip';
-          const dest = path.dirname('/tmp/ubuntu.zip');
-          await App.make(Downloader).download(url, '/tmp/ubuntu.zip');
-          const extral = await decompress(`/tmp/ubuntu.zip`, dest);
-          await mv(`${dest}/${extral[0].path}`, '/var/lib/mysql', { mkdirp: true });
-          await rimraf('/tmp/ubuntu.zip');
-          await rimraf(`${dest}/${extral[0].path}`);
+          await exec('apt-get -y update');
+          await exec('apt-get install -y mysql-server');
+          await exec('/etc/init.d/mysql start');
+          await exec('mysql_secure_installation');
         } else if ((await linux.CheckExists('mysql')) && fs.existsSync('/var/lib/mysql')) {
           dd(colors.green('your computer installed mysql - run "apt install mysql-server"'));
         }
