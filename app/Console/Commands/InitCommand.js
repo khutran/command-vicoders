3e1ef12a9d73e06ab6d633a1af95ec750999bdda
khutran <xuankhu1111@gmail.com>
2018-09-18 10:35:28 +0700

    test init

--- app/Console/Commands/InitCommand.js
+++ app/Console/Commands/InitCommand.js
@@ -94,7 +94,7 @@ export default class InitCommand extends Command {
         if (!fs.existsSync(`${user.homedir}/.npm/vcc/config.json`) && !(await lstat(`${__dirname}/../../config/config.json`)).isSymbolicLink()) {
           await mv(`${__dirname}/../../config/config.json`, `${user.homedir}/.npm/vcc/config.json`, { mkdirp: true });
           fs.symlinkSync(`${user.homedir}/.npm/vcc/config.json`, `${__dirname}/../../config/config.json`);
-        } else if (await lstat(`${__dirname}/../../config/config.json`).isSymbolicLink()) {
+        } else if ((await lstat(`${__dirname}/../../config/config.json`)).isSymbolicLink()) {
           console.log('test');
         } else {
           const answers = await inquirer.prompt({ type: 'confirm', name: 'config', message: 'Config exitis - you overwrite ?', default: false });
