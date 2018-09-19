3a104d49e6679e891f2bc8d34f039c5a899465ca
khutran <xuankhu1111@gmail.com>
2018-09-19 10:46:57 +0700

    test install httpd in centos 7

--- app/Utils/Installs/installApache.js
+++ app/Utils/Installs/installApache.js
@@ -49,57 +49,14 @@ export default class installAPache extends Install {
 
           config.apache.dir_conf = !_.isEmpty(config.apache.dir_conf) ? config.apache.dir_conf : '/usr/local/httpd/conf/extra/web';
           console.log('Install module ... !');
-          await exec('yum install -y gcc openssl-devel apr apr-util');
+          await exec('yum install -y gcc openssl-devel apr apr-util wget');
 
           console.log('Install apache... !');
-
-          const aliasName = 'centos';
-          const url = `https://github.com/khutran/${aliasName}-httpd/archive/${version}.zip`;
-          await App.make(Downloader).download(url, `/tmp/${version}.zip`);
-          const dest = path.dirname(`/tmp/${version}.zip`);
-          const extral = await decompress(`/tmp/${version}.zip`, dest);
-
-          if (fs.existsSync('/lib/systemd/system/httpd.service')) {
-            await rimraf('/lib/systemd/system/httpd.service');
-          }
-
-          if (fs.existsSync('/usr/sbin/httpd')) {
-            await rimraf('/usr/sbin/httpd');
-          }
-
-          if (fs.existsSync(config.apache.dir_etc)) {
-            await mv(config.apache.dir_etc, '/tmp/apache_old', { mkdirp: true });
-            await mv(`${dest}/${extral[0].path}`, config.apache.dir_etc, { mkdirp: true });
-            await rimraf(config.apache.dir_conf);
-            await rimraf('/usr/local/httpd/conf/httpd.conf');
-            await mv('/tmp/apache_old/conf/httpd.conf', `${config.apache.dir_etc}/conf/httpd.conf`, { mkdirp: true });
-            await mv('/tmp/apache_old/conf/extra/web', config.apache.dir_conf, { mkdirp: true });
-            await rimraf('/tmp/apache_old/');
-          } else {
-            console.log(config.apache.dir_etc);
-            await mv(`${dest}/${extral[0].path}`, config.apache.dir_etc, { mkdirp: true });
-          }
-          await mv(`${config.apache.dir_etc}/service/httpd.service`, '/lib/systemd/system/httpd.service', { mkdirp: true });
-
-          if (!fs.existsSync('/usr/sbin/httpd')) {
-            fs.symlinkSync(`${config.apache.dir_etc}/bin/httpd`, '/usr/sbin/httpd');
-          }
-
-          if (!fs.existsSync('/etc/systemd/system/multi-user.target.wants/httpd.service')) {
-            fs.symlinkSync('/lib/systemd/system/httpd.service', '/etc/systemd/system/multi-user.target.wants/httpd.service');
-          }
-
-          const passpd = fs.readFileSync('/etc/passwd');
-          if (passpd.indexOf('apache') === -1) {
-            await exec('useradd -s /sbin/nologin apache');
-          }
-
-          const data = JSON.stringify(config, null, 2);
-          fs.writeFileSync(`${__dirname}/../../config/config.json`, data);
-
-          await exec('systemctl daemon-reload');
-          await rimraf(`/tmp/${version}.zip`);
-          await rimraf(`${dest}/${extral[0].path}`);
+          await exec('yum install -y yum-changelog');
+          await exec('yum changelog httpd');
+          await exec('yum install -y epel-release');
+          await exec('wget https://repo.codeit.guru/codeit.el`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`.repo /etc/yum.repos.d');
+          await exec('yum install -y httpd');
         } catch (e) {
           throw new Exception(e.message, 1);
         }
